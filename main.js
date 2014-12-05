571b47be127d6aed8c78c5cded176cf549d486c1
takemura07 <k.0824.xoxo_@docomo.ne.jp>
2014-12-05 16:04:55 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,15 +2,17 @@
 var milkcocoa = new MilkCocoa("https://io-pi3159ro9.mlkcca.com");
 /* your-app-id にアプリ作成時に発行される"io-"から始まるapp-idを記入します */
 var chatDataStore = milkcocoa.dataStore("chat");
-var textArea, board;
+var textArea, board,textName;
+
 window.onload = function(){
   textArea = document.getElementById("msg");
   board = document.getElementById("board");
+  textName = document.getElementById("txtName");
 }
 
 function clickEvent(){
   var text = textArea.value;
-  sendText(text);
+ sendText(text,textName.value);
 }
 
 function sendText(text){
